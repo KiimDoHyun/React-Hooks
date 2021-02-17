d26f84e3202bf834bcb91d7c44bcde2ad94988be
KDH20160140 <bzidol@naver.com>
2021-02-17 16:49:47 +0900

    리액트 훅 useState 를 이용해 useTabs 구현

--- src/App.js
+++ src/App.js
@@ -1,49 +1,64 @@
 import React, { useState } from "react";
 
-// useInput("aas") : 입력값 aas 가 initialValue로 들어가서 useState에 의해 value 가 initialValue의 값으로 변경후 리턴됨.
-const useInput = (initialValue, validator) => {
-    const [value, setValue] = useState(initialValue);
-    const onChange = (event) => {
-        const {
-            target: { value }
-        } = event;
-        let willUpdate = true;
+const content = [
+    {
+        tab: "Section 1",
+        content: "I'm the content of the Section 1"
+    },
+    {
+        tab: "Section 2",
+        content: "I'm the content of the Section 2"
+    }
+];
 
-        //유효성 검사 함수
+/*
+두가지 값을 받는다 첫번째는 배열의 index, 두번째는 배열 자체
+ 
+*/
+const useTabs = (initialTab, allTabs) => {
+    //두번째 인자가 없거나, 배열이 아니면 바로 종료
+    const [currentIndex, setCurrentIndex] = useState(initialTab);
+    if (!allTabs || !Array.isArray(allTabs)) {
+        return;
+    }
 
-        //유효성 검사 인자변수 validator 로 들어오는 값이 함수인 경우 (유효성 검사는 함수 내에서 해야 하니깐)
-        //해당 함수의 결과 true or false 를 willUpdate 에 할당.
-        //유효성이 맞는 경우 willUpdate 는 자동으로 유효성함수에 의해 true 가 되고
-        // 이는 값을 변경 함.
-        if (typeof validator === "function") {
-            willUpdate = validator(value);
-        }
+    /*
+    useState 가 사용되었다. 마찬가지로 배열로 두가지 값을 가지고
+    currentIndex는 값, setCurrentIndex는 첫번째 인자 변경 방법이다.
+    선언시 initialTab을 받아서 currentIndex에 할당한다.
+    */
 
-        if (willUpdate) {
-            setValue(value);
-        }
+    /*
+    currentItem 과 changeItem 두가지를 반한다.
+    
+    첫번째 값은 allTabs (입력으로 들어온 배열) 의 
+    currentIndex (정상적으로 실행 되었다면 initialTab의 값이 currentIndex로 들어가있게된다.)
+    에 대항하는 배열 값을 반환한다. 
+    현재 allTabs 에 해당하는 것은 위에 선언한 content 배열이고
+    currentIndex 는 초기값으로 들어온 0 이기 때문에
+    currentItem 은 content[0]에 해당한다.
+  
+    두번쨰 값인 changeItem 은 setCurrentIndex 즉 변경 함수
+    changeItem 을 useTabs 선언시 사용 할수 있게 선언해 주고
+    onClick 에서 changeItem(index) 로 호출
+    입력값인 index 는 useState에 의해서 기존 value 에 해당하는 
+    currentIndex의 값을 변경함수 setCurrentIndex에 의해서 index 값으로 변경시킨다.
+    */
+    return {
+        currentItem: allTabs[currentIndex],
+        changeItem: setCurrentIndex
     };
-    return { value, onChange };
 };
 
 const App = () => {
-    const maxLen = (value) => value.length < 10;
-    // maxLen 은 문자열의 길이가 10 이하인 경우 true 를 반환한다.
-
-    const check = (value) => !value.includes("@");
-    //check 는 @ 를 포함하면 true, 없으면 false 를 반환
-    //반대 논리를 가지기 때문에 @는 입력 할 수 없다.
-
-    //이처럼 문자열 길이 이외에도 다른 형태의 검증 함수를 이용해도 상관없다.
-    const name = useInput("Mr.", check);
-
-    //실제 작동시 유효성 검사에 의해 문자열의 길이가 10이 넘어가면
-    //더이상 입력 할 수 없다.
+    const { currentItem, changeItem } = useTabs(0, content);
     return (
         <div className="App">
             <h1>Hello</h1>
-            {/* <input placeholder="Name" value={name.value} onChange={name.onChange}/> 아래랑 똑같음*/}
-            <input placeholder="Name" {...name} />
+            {content.map((section, index) => (
+                <button onClick={() => changeItem(index)}>{section.tab}</button>
+            ))}
+            <div>{currentItem.content}</div>
         </div>
     );
 };
